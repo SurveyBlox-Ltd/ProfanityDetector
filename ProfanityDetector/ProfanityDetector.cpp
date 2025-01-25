d16086256c0448692435eb615e4e7af086740244
Shayden seymour <shaydenseymour@gmail.com>
2025-01-26 02:56:11 +1300

    ProfanityDetector

---
+++ ProfanityDetector/ProfanityDetector.cpp
@@ -0,0 +1,37 @@
+﻿/**
+ * @fileProfanityDetector.cpp
+ * 
+ * @description Checks messages for profanity
+ * 
+ * @copyright SurveyBlox Ltd & Shayden Seymour
+ */
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <ios>
+
+namespace BProfanity {
+	namespace FileSystem {
+		namespace SSTable {
+			void write(const std::string& tablePath, const std::string& content) {
+				std::ofstream dataStream(tablePath, std::ios::binary);
+			}
+		} // namespace SSTable
+
+		void loadBlacklistsWordFile() {
+			std::fstream fileStream;
+
+			fileStream.open("data/blacklistedWords.txt");
+		}
+	} // namespace FileSystem
+
+	bool check(std::string& content) {
+		if (content.empty()) {
+			return false;
+		}
+
+		std::transform(content.begin(), content.end(), content.begin(), ::tolower);
+	}
+}
---
+++ ProfanityDetector/ProfanityDetector.h
@@ -0,0 +1,16 @@
+﻿#ifndef B_PROFANITY_DETECTOR
+#define B_PROFANITY_DETECTOR
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+namespace BProfanity {
+	/**
+		* @brief Checks if the message contains a blacklisted word, returns true or false.
+		* @param std::string content The message you want to check 
+		*/
+	bool check(std::string& content);
+}
+
+#endif // B_PROFANITY_DETECTOR
